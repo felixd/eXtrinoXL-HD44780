4680948898ac0bcf04ef239a1634ea6168685217
Paweł 'felixd' Wojciechowski <felixd@konopnickiej.com>
2018-01-25 20:33:15 +0100

    Inicjalizacja

---
+++ README.md
@@ -0,0 +1,19 @@
+# eXtrinoXL-HD44780
+Biblioteka obsługująca wyświetlacz alfanumeryczny ze sterownikiem HD44780. Sterowanie odbywa się w trybie 4-bitowym, bez odczytu flagi zajętości z dowolnym przypisaniem sygnałów sterujących. 
+
+Biblioteka przygotowana jest do obsługi mikrokontrolera Atmel AVR XMEGA128A3U znajdującego się na płytce eXtrino XL FULL wyprodukowanej przez Leon Instruments.
+
+
+## Źródła
+* [Kurs na stronie Leon Instruments](https://extronic.pl/content/48-kurs-xmega-wyswietlacz-lcd)
+* [Oryginalna biblioteka HD44780 autorstwa Radosława Kwietnia](http://radzio.dxp.pl)
+
+## Autorzy
+* Radosław Kwiecień - <http://radzio.dxp.pl>
+* Dominik Bieczyński (Leon Instruments) - <https://extronic.pl>
+* Paweł 'felixd' Wojciechowski - <http://www.konopnickiej.com>
+
+## Edytor
+Program edytowany był i kompilowany za pomocą środowiska Microsoft Visual Studio Code oraz Platformio IO
+Kodowanie: UTF-8
+Znak końca linii: Unix LF
---
+++ src/main.c
@@ -0,0 +1,43 @@
+/*
+  Dominik Leon Bieczyński
+  Leon Instruments
+  http://leon-instruments.blogspot.com
+
+  Data: 24.01.2018
+  Dostowanie do płytki eXtrinoXL FULL [Atmel AVR XMEGA128A3U]
+  Paweł 'felixd' Wojciechowski
+  Outsourcing IT - Konopnickiej.Com
+  http://www.konopnickiej.com
+ */
+
+#define F_CPU 2000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include "hd44780.h"
+
+int main(void) {
+
+	// konfiguracja przycisku FLIP
+	PORTE.DIRCLR = PIN5_bm;					// pin E5 jako wejście
+	PORTE.PIN5CTRL = PORT_OPC_PULLUP_gc;	// podciągnięcie do zasilania
+
+	// Inicjalizacja wyświetlacza
+	LcdInit();								// inicjalizacja sterownika LCD
+	Lcd("   eXtrino XL   ");				// wyświetlenie napisu
+	Lcd2;									// przejście do drugiej linii
+	Lcd("Konopnickiej.Com");				// wyświetlenie napisu
+
+	// zmienna służąca jako licznik
+	uint8_t licznik = 0;
+
+	while (1) {
+		if (!(PORTE.IN & PIN5_bm)) {		// jeżeli przycisk FLIP jest wciśnięty
+			licznik++;						// zwiększ licznik o 1
+			LcdClear();						// wyczyśc wyświetlacz
+			Lcd("Licznik:");				// wyświetlenie napisu
+			Lcd2;							// przejście do drugiej linii
+			LcdDec(licznik);				// wyświetlenie zmiennej liczbowej
+			_delay_ms(100);					// czekanie 100ms
+		}
+	}
+}
