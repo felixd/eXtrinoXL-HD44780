a800bf14d4e97a98705b1e2659e42bdb507272ab
Paweł 'felixd' Wojciechowski <felixd@konopnickiej.com>
2018-01-25 21:27:03 +0100

    Formatowanie kodu + integracja z Platformio IO

--- src/hd44780.h
+++ src/hd44780.h
@@ -24,7 +24,7 @@
 #ifndef HD44780_H_
 #define HD44780_H_
 
-#define F_CPU 2000000UL
+// #define F_CPU 2000000UL
 
 #include <avr/io.h>
 #include <util/delay.h>
@@ -36,20 +36,20 @@
 // Sekcja została dostosowana do płytki eXtrinoXL FULL
 //-------------------------------------------------------------------------------------------------
 
-#define LCD_RS_PORT		PORTE
-#define LCD_RS_bm		PIN4_bm	// E4
+#define LCD_RS_PORT PORTE
+#define LCD_RS_bm PIN4_bm // E4
 
-#define LCD_PORT		PORTF
-#define LCD_E_bm		PIN3_bm	// F3
-#define LCD_D4_bm		PIN4_bm	// F4
-#define LCD_D5_bm		PIN5_bm	// F5
-#define LCD_D6_bm		PIN6_bm	// F6
-#define LCD_D7_bm		PIN7_bm	// F7
+#define LCD_PORT PORTF
+#define LCD_E_bm PIN3_bm  // F3
+#define LCD_D4_bm PIN4_bm // F4
+#define LCD_D5_bm PIN5_bm // F5
+#define LCD_D6_bm PIN6_bm // F6
+#define LCD_D7_bm PIN7_bm // F7
 
 // definicje ulatwiajace pisanie tekstow
-#define Lcd(tekst) 	    LcdWriteProgmem(PSTR(tekst))
-#define Lcd1			LcdGoto(0,0)					// skok do pierwszej linii
-#define Lcd2			LcdGoto(0,1)					// skok do drugiej linii
+#define Lcd(tekst) LcdWriteProgmem(PSTR(tekst))
+#define Lcd1 LcdGoto(0, 0) // skok do pierwszej linii
+#define Lcd2 LcdGoto(0, 1) // skok do drugiej linii
 
 //-------------------------------------------------------------------------------------------------
 //
@@ -57,38 +57,38 @@
 //
 //-------------------------------------------------------------------------------------------------
 
-#define HD44780_CLEAR				    0x01
-#define HD44780_HOME				    0x02
-#define HD44780_ENTRY_MODE			    0x04
-#define HD44780_EM_SHIFT_CURSOR		    0
-#define HD44780_EM_SHIFT_DISPLAY        1
-#define HD44780_EM_DECREMENT		    0
-#define HD44780_EM_INCREMENT		    2
+#define HD44780_CLEAR 0x01
+#define HD44780_HOME 0x02
+#define HD44780_ENTRY_MODE 0x04
+#define HD44780_EM_SHIFT_CURSOR 0
+#define HD44780_EM_SHIFT_DISPLAY 1
+#define HD44780_EM_DECREMENT 0
+#define HD44780_EM_INCREMENT 2
 
-#define HD44780_DISPLAY_ONOFF		    0x08
-#define HD44780_DISPLAY_OFF			    0
-#define HD44780_DISPLAY_ON			    4
-#define HD44780_CURSOR_OFF			    0
-#define HD44780_CURSOR_ON			    2
-#define HD44780_CURSOR_NOBLINK		    0
-#define HD44780_CURSOR_BLINK			1
+#define HD44780_DISPLAY_ONOFF 0x08
+#define HD44780_DISPLAY_OFF 0
+#define HD44780_DISPLAY_ON 4
+#define HD44780_CURSOR_OFF 0
+#define HD44780_CURSOR_ON 2
+#define HD44780_CURSOR_NOBLINK 0
+#define HD44780_CURSOR_BLINK 1
 
-#define HD44780_DISPLAY_CURSOR_SHIFT	0x10
-#define HD44780_SHIFT_CURSOR			0
-#define HD44780_SHIFT_DISPLAY		    8
-#define HD44780_SHIFT_LEFT			    0
-#define HD44780_SHIFT_RIGHT			    4
+#define HD44780_DISPLAY_CURSOR_SHIFT 0x10
+#define HD44780_SHIFT_CURSOR 0
+#define HD44780_SHIFT_DISPLAY 8
+#define HD44780_SHIFT_LEFT 0
+#define HD44780_SHIFT_RIGHT 4
 
-#define HD44780_FUNCTION_SET			0x20
-#define HD44780_FONT5x7				    0
-#define HD44780_FONT5x10				4
-#define HD44780_ONE_LINE				0
-#define HD44780_TWO_LINE				8
-#define HD44780_4_BIT				    0
-#define HD44780_8_BIT				    16
+#define HD44780_FUNCTION_SET 0x20
+#define HD44780_FONT5x7 0
+#define HD44780_FONT5x10 4
+#define HD44780_ONE_LINE 0
+#define HD44780_TWO_LINE 8
+#define HD44780_4_BIT 0
+#define HD44780_8_BIT 16
 
-#define HD44780_CGRAM_SET			    0x40
-#define HD44780_DDRAM_SET			    0x80
+#define HD44780_CGRAM_SET 0x40
+#define HD44780_DDRAM_SET 0x80
 
 //-------------------------------------------------------------------------------------------------
 //
@@ -98,8 +98,8 @@
 
 void LcdCommand(unsigned char commandToWrite);
 void LcdData(unsigned char data);
-void LcdWrite(char text);
-void LcdWriteProgmem(const char data);
+void LcdWrite(char *text);
+void LcdWriteProgmem(const char *data);
 void LcdGoto(unsigned char x, unsigned char y);
 void LcdClear(void);
 void LcdInit(void);
--- src/main.c
+++ src/main.c
@@ -15,29 +15,32 @@
 #include <util/delay.h>
 #include "hd44780.h"
 
-int main(void) {
+int main(void)
+{
 
 	// konfiguracja przycisku FLIP
-	PORTE.DIRCLR = PIN5_bm;					// pin E5 jako wejście
-	PORTE.PIN5CTRL = PORT_OPC_PULLUP_gc;	// podciągnięcie do zasilania
+	PORTE.DIRCLR = PIN5_bm;				 // pin E5 jako wejście
+	PORTE.PIN5CTRL = PORT_OPC_PULLUP_gc; // podciągnięcie do zasilania
 
 	// Inicjalizacja wyświetlacza
-	LcdInit();								// inicjalizacja sterownika LCD
-	Lcd("   eXtrino XL   ");				// wyświetlenie napisu
-	Lcd2;									// przejście do drugiej linii
-	Lcd("Konopnickiej.Com");				// wyświetlenie napisu
+	LcdInit();				 // inicjalizacja sterownika LCD
+	Lcd("   eXtrino XL   "); // wyświetlenie napisu
+	Lcd2;					 // przejście do drugiej linii
+	Lcd("Konopnickiej.Com"); // wyświetlenie napisu
 
 	// zmienna służąca jako licznik
 	uint8_t licznik = 0;
 
-	while (1) {
-		if (!(PORTE.IN & PIN5_bm)) {		// jeżeli przycisk FLIP jest wciśnięty
-			licznik++;						// zwiększ licznik o 1
-			LcdClear();						// wyczyśc wyświetlacz
-			Lcd("Licznik:");				// wyświetlenie napisu
-			Lcd2;							// przejście do drugiej linii
-			LcdDec(licznik);				// wyświetlenie zmiennej liczbowej
-			_delay_ms(100);					// czekanie 100ms
+	while (1)
+	{
+		if (!(PORTE.IN & PIN5_bm))
+		{					 // jeżeli przycisk FLIP jest wciśnięty
+			licznik++;		 // zwiększ licznik o 1
+			LcdClear();		 // wyczyśc wyświetlacz
+			Lcd("Licznik:"); // wyświetlenie napisu
+			Lcd2;			 // przejście do drugiej linii
+			LcdDec(licznik); // wyświetlenie zmiennej liczbowej
+			_delay_ms(100);  // czekanie 100ms
 		}
 	}
 }
